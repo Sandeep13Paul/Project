21b813169432851efdcd477256971b1531f1e489
Abhibhava <manantyagi23@gmail.com>
2024-04-08 15:04:28 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ app.use('/', (req,res)=>{
     res.send('first route');
 })
 
+app.use('/hello', (req,res)=>{
+    res.send("Hello everyone");
+})
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Listening to Port: ${process.env.PORT}`);
 })
